e645107204da18ff947cd4ca9413543058a1948d
Patrik Dóczy <doczy.patrik410@gmail.com>
2024-10-19 18:18:50 +0200

    project initialization

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+    root: true,
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint', 'prettier'],
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:prettier/recommended',
+    ],
+    env: {
+        node: true,
+        es6: true,
+    },
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+    },
+    rules: {
+        'prettier/prettier': 'error',
+        '@typescript-eslint/no-unused-vars': 'warn',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/no-explicit-any': 'warn',
+    },
+    overrides: [
+        {
+            files: ['*.ts', '*.tsx'],
+            rules: {
+                '@typescript-eslint/explicit-module-boundary-types': 'off',
+            },
+        },
+    ],
+    ignorePatterns: ['node_modules/', 'dist/', '.next/', 'build/', 'coverage/'],
+};
