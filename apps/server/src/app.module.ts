c301f628fea4f1f5f59e0a23af18f278de7cd0dc
Patrik Dóczy <doczy.patrik410@gmail.com>
2024-10-20 10:29:47 +0200

    prisma service added

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -5,10 +5,11 @@ import { FinnhubService } from './finnhub/finnhub.service';
 import { HttpModule } from '@nestjs/axios';
 import { FinnhubController } from './finnhub/finnhub.controller';
 import { ConfigModule } from '@nestjs/config';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [ConfigModule.forRoot(), HttpModule],
   controllers: [AppController, FinnhubController],
-  providers: [AppService, FinnhubService],
+  providers: [AppService, FinnhubService, PrismaService],
 })
 export class AppModule { }
